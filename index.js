d3a9e01c8820efcb88d16e246fb7d5575f126d1a
Hammad19 <56793093+Hammad19@users.noreply.github.com>
2023-03-19 13:27:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 import dotenv from "dotenv";
 dotenv.config();
+import bodyParser from "body-parser";
 import cors from "cors";
 import express from "express";
 import dbConnection from "./configs/db_connection.js";
 import userRoutes from "./routes/UserRoutes.js";
 import foodRoutes from "./routes/FoodRoutes.js";
 import orderRoutes from "./routes/OrderRoutes.js";
+import imageRoutes from "./routes/ImageRoutes.js";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Middlewares
 // CORS POLICY
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(cors());
 // JSON PARSER
 app.use(express.json());
@@ -19,7 +27,9 @@ app.use(express.json());
 dbConnection(process.env.DATABASE_URL);
 
 // ROUTES
+app.use("/public", express.static("public"));
 
+app.use("/api/images", imageRoutes);
 app.use("/api/food", foodRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
