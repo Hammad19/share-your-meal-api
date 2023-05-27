cf8ff35b961d416b10f9e0b092a34413686be916
Hasnain Mohal <56793093+Hammad19@users.noreply.github.com>
2023-05-27 16:33:40 +0500

    update

--- routes/NotificationRoutes.js
+++ routes/NotificationRoutes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { getNotifications } from "../controllers/NotificationController";
 
 const router = express.Router();
+
+router.route("/:user_email").get(getNotifications);
 
 export default router;
